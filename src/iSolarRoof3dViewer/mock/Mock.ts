42142c5aa64bf49e31a71cd55fd06143f67caa86
mzli11 <mzli11@iflytek.com>
2024-12-22 21:24:19 +0800

    FIX:模拟数据

--- src/iSolarRoof3dViewer/mock/Mock.ts
+++ src/iSolarRoof3dViewer/mock/Mock.ts
@@ -1,87 +1,346 @@
 
+// const data = {
+//     "areas": [
+
+//         {
+//             "pid": "4z3izey",
+//             "points": [
+//                 "69zdg6z",
+//                 "dxz6qhj",
+//                 "vlncftq",
+//                 "4lhxmv6"
+//             ]
+//         },
+//         {
+//             "pid": "h7q0qyw",
+//             "points": [
+//                 "wmz299i",
+//                 "2uumzk0",
+//                 "vlncftq",
+//                 "dxz6qhj"
+//             ]
+//         }
+//     ],
+//     "points": [
+//         {
+//             "pid": "4lhxmv6",
+//             "point": [
+//                 0.143,
+//                 0.281,
+//                 0.201
+//             ]
+//         },
+//         {
+//             "pid": "69zdg6z",
+//             "point": [
+//                 0.129,
+//                 0.439,
+//                 0.201
+//             ]
+//         },
+//         {
+//             "pid": "vlncftq",
+//             "point": [
+//                 0.284,
+//                 0.267,
+//                 0.201
+//             ]
+//         },
+//         {
+//             "pid": "dxz6qhj",
+//             "point": [
+//                 0.35,
+//                 0.471,
+//                 0.201
+//             ]
+//         },
+//         {
+//             "pid": "2uumzk0",
+//             "point": [
+//                 0.477,
+//                 0.272,
+//                 0.201
+//             ]
+//         },
+//         {
+//             "pid": "wmz299i",
+//             "point": [
+//                 0.517,
+//                 0.625,
+//                 0.201
+//             ]
+//         }
+//     ],
+//     "lines": [
+//         {
+//             "id": "cq198ng",
+//             "points": [
+//                 "4lhxmv6",
+//                 "69zdg6z"
+//             ]
+//         },
+//         {
+//             "id": "o8ldc3c",
+//             "points": [
+//                 "69zdg6z",
+//                 "dxz6qhj"
+//             ]
+//         },
+//         {
+//             "id": "osiz34z",
+//             "points": [
+//                 "dxz6qhj",
+//                 "wmz299i"
+//             ]
+//         },
+//         {
+//             "id": "1v5jkog",
+//             "points": [
+//                 "wmz299i",
+//                 "2uumzk0"
+//             ]
+//         },
+//         {
+//             "id": "jsqsvll",
+//             "points": [
+//                 "2uumzk0",
+//                 "vlncftq"
+//             ]
+//         },
+//         {
+//             "id": "imvg1e9",
+//             "points": [
+//                 "vlncftq",
+//                 "4lhxmv6"
+//             ]
+//         },
+//         {
+//             "id": "6koy7lk",
+//             "points": [
+//                 "vlncftq",
+//                 "dxz6qhj"
+//             ]
+//         }
+//     ],
+//     "roof": [
+//         "69zdg6z",
+//         "dxz6qhj",
+//         "wmz299i",
+//         "2uumzk0",
+//         "vlncftq",
+//         "4lhxmv6"
+//     ]
+// }
+
 const data = {
+
     "areas": [
         {
-            "pid": "oqdgd06",
+            "pid": "m60eqig",
             "points": [
-                "hp6mmci",
-                "6vznskw",
-                "8oftlt3",
-                "nbbhe87"
+                "ky2szej",
+                "ty884z0",
+                "gnwtehc",
+                "kvxegsm"
+            ]
+        },
+        {
+            "pid": "nq4g0ug",
+            "points": [
+                "gnwtehc",
+                "5p19n1s",
+                "pynut9b",
+                "ty884z0"
+            ]
+        },
+        {
+            "pid": "a7kcdmg",
+            "points": [
+                "rvhyaub",
+                "kvxegsm",
+                "ky2szej",
+                "adxzpna"
+            ]
+        },
+        {
+            "pid": "jzspfms",
+            "points": [
+                "kvxegsm",
+                "gnwtehc",
+                "5p19n1s",
+                "rvhyaub"
+            ]
+        },
+        {
+            "pid": "ilqjmjw",
+            "points": [
+                "pynut9b",
+                "ty884z0",
+                "ky2szej",
+                "adxzpna"
             ]
         }
     ],
     "points": [
         {
-            "pid": "nbbhe87",
+            "pid": "adxzpna",
             "point": [
-                0.109,
-                0.2,
+                0.128,
+                0.259,
                 0.201
             ]
         },
         {
-            "pid": "8oftlt3",
+            "pid": "rvhyaub",
             "point": [
-                0.149,
-                0.382,
+                0.133,
+                0.503,
                 0.201
             ]
         },
         {
-            "pid": "hp6mmci",
+            "pid": "5p19n1s",
             "point": [
-                0.379,
-                0.164,
+                0.533,
+                0.499,
                 0.201
             ]
         },
         {
-            "pid": "6vznskw",
+            "pid": "pynut9b",
             "point": [
-                0.483,
-                0.571,
+                0.527,
+                0.246,
+                0.201
+            ]
+        },
+        {
+            "pid": "ky2szej",
+            "point": [
+                0.257,
+                0.369,
+                0.201
+            ]
+        },
+        {
+            "pid": "ty884z0",
+            "point": [
+                0.447,
+                0.368,
+                0.201
+            ]
+        },
+        {
+            "pid": "kvxegsm",
+            "point": [
+                0.251,
+                0.42,
+                0.201
+            ]
+        },
+        {
+            "pid": "gnwtehc",
+            "point": [
+                0.451,
+                0.422,
                 0.201
             ]
         }
     ],
     "lines": [
         {
-            "id": "heojrme",
+            "id": "mooktvc",
             "points": [
-                "nbbhe87",
-                "hp6mmci"
+                "adxzpna",
+                "rvhyaub"
             ]
         },
         {
-            "id": "lns9g97",
+            "id": "8kgqk2o",
             "points": [
-                "hp6mmci",
-                "6vznskw"
+                "rvhyaub",
+                "kvxegsm"
             ]
         },
         {
-            "id": "0cbzel4",
+            "id": "zqz7w23",
             "points": [
-                "6vznskw",
-                "8oftlt3"
+                "kvxegsm",
+                "ky2szej"
             ]
         },
         {
-            "id": "io17r4d",
+            "id": "k2u2ncc",
             "points": [
-                "8oftlt3",
-                "nbbhe87"
+                "ky2szej",
+                "ty884z0"
+            ]
+        },
+        {
+            "id": "kcumsy8",
+            "points": [
+                "ty884z0",
+                "gnwtehc"
+            ]
+        },
+        {
+            "id": "k3ym5ce",
+            "points": [
+                "gnwtehc",
+                "kvxegsm"
+            ]
+        },
+        {
+            "id": "qhb67ib",
+            "points": [
+                "5p19n1s",
+                "rvhyaub"
+            ]
+        },
+        {
+            "id": "kcogb0s",
+            "points": [
+                "gnwtehc",
+                "5p19n1s"
+            ]
+        },
+        {
+            "id": "e8zfp8v",
+            "points": [
+                "ty884z0",
+                "pynut9b"
+            ]
+        },
+        {
+            "id": "muj1kyt",
+            "points": [
+                "pynut9b",
+                "adxzpna"
+            ]
+        },
+        {
+            "id": "wt2hmzl",
+            "points": [
+                "adxzpna",
+                "ky2szej"
+            ]
+        },
+        {
+            "id": "odxe2kr",
+            "points": [
+                "pynut9b",
+                "5p19n1s"
             ]
         }
     ],
     "roof": [
-        "hp6mmci",
-        "6vznskw",
-        "8oftlt3",
-        "nbbhe87"
+        "rvhyaub",
+        "5p19n1s",
+        "pynut9b",
+        "adxzpna"
     ]
 }
+
 export {
     data
 }
